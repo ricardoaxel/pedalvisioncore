79c77b05b02b739d776bb58758e5343fc5ea2109
Ricardo Garcia <ricardo.garcia@octopy.com>
2022-06-07 19:16:10 -0500

    correction on directories

--- pedalvision/src/App.jsx
+++ pedalvision/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "./Layouts/Layout";
-import { PedalboardView } from "./Views/PedalboardView/PedalboardView";
+import { PedalboardView } from "./views/PedalboardView/PedalboardView";
 import { SessionProvider } from "./Contexts/sessionContext/sessionContext";
 export const App = () => {
   return (
